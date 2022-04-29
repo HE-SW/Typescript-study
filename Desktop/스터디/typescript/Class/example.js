eb06af9e473ea81897489dea57ff30de409d57f3
han_eol <gksdjf80@naver.com>
2022-04-29 18:44:49 +0900

    타입스크립트스터디

---
+++ Desktop/스터디/typescript/Class/example.js
@@ -0,0 +1,8 @@
+"use strict";
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+}
+const p1 = new Person("Marks");
+console.log(p1);
---
+++ Desktop/스터디/typescript/Class/example.ts
@@ -0,0 +1,11 @@
+class Person {
+  name;
+  constructor(name: string) {
+    this.name = name;
+    
+  }
+}
+
+const p1 = new Person("Marks")
+
+console.log(p1)
